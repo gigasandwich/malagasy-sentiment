346f7627e70a93cd523528a47a5003aeb74a6724
Yvan Noah <yvannandy@gmail.com>
2025-03-22 11:03:29 +0300

    Add readme.txt

--- format.py
+++ format.py
@@ -3,8 +3,9 @@ filePath = 'data/original.txt'
 with open(filePath, 'r') as file:
     count = 0
     for line in file:
+        line = line.strip()
         print(line)
         
         count += 1
         if count == 20:
-            break   
+            break
