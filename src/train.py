2f096e641c3d3345effb9a63ab8a0d4adbd646fd
Yvan Noah <yvannandy@gmail.com>
2025-03-22 12:57:08 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,9 +1,15 @@
 from src.variables import *
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def main():
     df = load_data(f'{datafolder}/english.csv')
 
+    X = df['comment'].values
+    y = df['sentiment'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
 def load_data(filepath: str):
     df = pd.read_csv(filepath)
     return df
