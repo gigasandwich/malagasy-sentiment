51c7413d3687dae0d5e3f052e26f7fa2af0b43dc
Yvan Noah <yvannandy@gmail.com>
2025-03-22 21:03:02 +0300

    Add get_classification method

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 from src.variables import *
-from src.vectorizer import Vectorizer, BOW, TFIDF
+from src.vectorizer import Vectorizer, BOW, TFIDF, WORD_EMBEDDINGS
+from src.classification_model import Model, Naive_Bayes, LogisticRegression, RandomForest
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from typing import Literal
@@ -7,20 +9,40 @@ from typing import Literal
 def main():
     '''
     Here, we: \n
-    - Choose a vectorizer with the get_vectorizer method \n
-    - Train the chosen model with the X_vectorized variable
+    1) Choose a vectorizer with the get_vectorizer method \n
+    2) Vectorize X_train with the X_vectorized variable
+    3) Choose a model with the get_classification_model method
     '''
     X_train, X_test, y_train, y_test = get_splitted_train_test()
     
+    # 1
     vectorizer = get_vectorizer('BOW') # Just change the argument if you want to test another one
+    
+    # 2
     X_vectorized = vectorizer.vectorize(X_train)
-    print(X_vectorized)
+    
+    # 3
+    model = get_classification_method('Naive_Bayes')
+    model.fit(X_vectorized, y_train)
+    
+    # Evaluate the model
+    y_pred = model.pre    
+
+def get_classification_method(string: Literal['Naive_Bayes', 'LogisticRegression', 'RandomForest']) -> Model:
+    if string == 'Naive_Bayes':
+        return Naive_Bayes()
+    if string == 'LogisticRegression':
+        return LogisticRegression()
+    if string == 'RandomForest':
+        return RandomForest()
 
 def get_vectorizer(string: Literal['BOW', 'TFIDF', 'WORD_EMBEDDINGS']) -> Vectorizer:
     if string == 'BOW':
         return BOW()
     if string == 'TFIDF':
         return TFIDF()
+    if string == 'WORD_EMBEDDINGS':
+        return WORD_EMBEDDINGS()
 
 def get_splitted_train_test():
     df = load_data(f'{datafolder}/english.csv')
